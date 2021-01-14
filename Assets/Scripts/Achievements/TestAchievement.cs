5aefc7bdd44f9a4184d4271b9fff3c02ddc6adab
Joel <fallendemongod@gmail.com>
2021-01-14 00:53:18 -0500

    Test Achievement Completetion

--- Assets/Scripts/Achievements/TestAchievement.cs
+++ Assets/Scripts/Achievements/TestAchievement.cs
@@ -9,11 +9,12 @@ namespace Achievements
         public AchievementTemplate test;
 
         public Achievement testAchievement;
-        
-        Dictionary<>
+
+        private Dictionary<string, Achievement> achievements = new Dictionary<string, Achievement>();
         private void Start()
         {
             testAchievement = new Achievement(test.title, test.icon, test.description, test.obj, test.amount, test.condition);
+            achievements.Add(testAchievement.name, testAchievement);
         }
 
         // Update is called once per frame
@@ -21,14 +22,21 @@ namespace Achievements
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                CheckAchievement();
+                CheckAchievement(AchievementTemplate.AchievementCondition.Kill, "boar", 1);
             }
 
         }
         
-        void CheckAchievement(AchievementTemplate.AchievementCondition condition, string name, int amount)
+        void CheckAchievement(AchievementTemplate.AchievementCondition condition,string name, int amount)
         {
-            
+            if (achievements.TryGetValue(name, out var achievement))
+            {
+                if (achievement.condition == condition && !achievement.completed)
+                {
+                    achievement.
+                }
+                else return;
+            }
         }
     }
 }
